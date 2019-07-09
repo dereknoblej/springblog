bb675b614190997faccfa3336f371dc85b010f0c
dereknoblej <derek.noblej@gmail.com>
2019-07-09 11:45:49 -0500

    fixed another issue

--- src/main/java/com/codeup/springblog/AuthenticationController.java
+++ src/main/java/com/codeup/springblog/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
-    @GetMapping("/login2")
+    @GetMapping("/login")
     public String showLoginForm() {
         return "users/login";
     }
