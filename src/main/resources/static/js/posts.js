f239f45c19148b3b469639019dea918cd3c79717
dereknoblej <derek.noblej@gmail.com>
2019-09-27 16:32:34 -0500

    updateing search function

--- src/main/resources/static/js/posts.js
+++ src/main/resources/static/js/posts.js
@@ -1,26 +1,47 @@
 "use strict";
 
 (function($) {
-    var request = $.ajax({'url': '/posts.json'});
-    request.done(function (posts) {
-        var html = '';
-        var postsArray = [];
-        posts.forEach(function(post) {
-            html += '<div class = "posts">';
-            html += '<h1>' + post.title + '</h1>';
-            html += '<p>' + post.body + '</p>';
-            html += '<p>Published by ' + post.author.username + '</p>';
-            html += '</div>';
 
-            postsArray.push({id:post.id, name:post.title, body:post.body, author:post.author.username},)
+    var arrayPosts = [];
+
+    var postsArray = $.ajax({'url': '/posts.json'});
+     postsArray.done(function (posts) {
+        var html = '';
+
+        posts.forEach(function(post) {
+            // html += '<div class = "posts">';
+            // html += '<h1>' + post.title + '</h1>';
+            // html += '<p>' + post.body + '</p>';
+            // html += '<p>Published by ' + post.author.username + '</p>';
+            // html += '</div>';
+
+            arrayPosts.push({id:post.id, name:post.title, body:post.body, author:post.author.username},)
 
 
         });
-        $('#posts').html(html);
+        // $('#posts').html(html);
+
+        return postsArray;
     });
+
+     console.log(arrayPosts);
+function filterPosts(){
+
+    arrayPosts.forEach(function(posts, i) {
+
+        console.log(posts)
+        console.log(i);
+
+    });
+
+}
+
+filterPosts();
+
+
 })(jQuery);
 
-function filterPosts() {
-    
-}
+
+
+
 
