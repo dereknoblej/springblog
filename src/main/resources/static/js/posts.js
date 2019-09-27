43d419cd753a0d401a6cc1b4a896be855a02235f
dereknoblej <derek.noblej@gmail.com>
2019-09-27 14:57:36 -0500

    working on javascript search function

--- src/main/resources/static/js/posts.js
+++ src/main/resources/static/js/posts.js
@@ -1,18 +1,26 @@
-
-
+"use strict";
 
 (function($) {
     var request = $.ajax({'url': '/posts.json'});
     request.done(function (posts) {
         var html = '';
+        var postsArray = [];
         posts.forEach(function(post) {
-            html += '<div>';
+            html += '<div class = "posts">';
             html += '<h1>' + post.title + '</h1>';
             html += '<p>' + post.body + '</p>';
             html += '<p>Published by ' + post.author.username + '</p>';
             html += '</div>';
+
+            postsArray.push({id:post.id, name:post.title, body:post.body, author:post.author.username},)
+
+
         });
         $('#posts').html(html);
     });
 })(jQuery);
+
+function filterPosts() {
+    
+}
 
